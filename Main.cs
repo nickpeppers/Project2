94d5cedc05af75c88c17861a603cffcd85a6beed
Nick Peppers <nick.peppers@hitcents.com>
2012-10-11 17:15:47 -0500

    Setup threading

--- Main.cs
+++ Main.cs
@@ -1,57 +1,49 @@
 using System;
 using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Project2
 {
-	class MainClass
+	static class MainClass
 	{
-		int [,]ArrayA = new int[,] {{1,1},{2,2},{3,3},{4,4}};
-		int [,]ArrayB = new int[,] {{5,4,3,2},{6,7,8,9}};
-		int [,]ArrayC;
-
-		public class pThread
-		{
-			// Called when the thread is started.
-			public void ThreadWork()
-			{
-				while (!_StopThread)
-				{
-
-				}
-			}
-			public void RequestStop()
-			{
-				_StopThread = true;
-			}
-
-			private volatile bool _StopThread;
-		}
-
+		static int [,]ArrayA = new int[,] {{1,1},{2,2},{3,3},{4,4}};
+		static int [,]ArrayB = new int[,] {{5,4,3,2},{6,7,8,9}};
+		static int [,]ArrayC;
 
 		public static void Main (string[] args)
 		{
 
 			while (true)
 			{
-				pThread  ThreadMatrix = new pThread();
-				Thread workerThread = new Thread(ThreadMatrix.ThreadWork);
+                var tasks = new List<Task<int>>();
 
-				// Starts thread
-				workerThread.Start();
+                for (int i = 0; i < ArrayA.GetLength(0); i++)
+                {
+                    for (int j = 0; j < ArrayB.GetLength(1); j++)
+                    {
+                        tasks.Add(Task.Factory.StartNew(() => CalculateColumn(i, j)));
+                    }
+                }
 
-				// Loops until thread activates.
-				while (!workerThread.IsAlive);
-				
-				// Put the main thread to sleep for 1 millisecond to
-				// allow thread to do some work:
-				Thread.Sleep(1);
-				
-				// Request the thread stop itself:
-				ThreadMatrix.RequestStop();
-				
-				// Join method blocks the current thread until the object's thread terminates
-				workerThread.Join();
+                //Block until all tasks complete.
+                Task.WaitAll(tasks.ToArray());
+
+                //see results of the task
+                for (int i = 0; i < tasks.Count; i++)
+                {
+                    Console.WriteLine(i + " " + tasks[i].Result);
+                }
 			}
 		}
+
+        private static int CalculateColumn(int i, int j)
+        {
+            return 0;
+            //for (int k = 0; k < ArrayB.GetLength; k++)
+            //{
+            //    ArrayC[i, j] += ArrayA[i, k] * ArrayB[k, j];
+            //}
+        }
 	}
 }
