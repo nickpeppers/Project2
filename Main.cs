56f2cd444b231e0780344acd47233fdecc076151
Nick Peppers <nick.peppers@hitcents.com>
2012-10-16 14:44:07 -0500

    Finished matrix multiplication

--- Main.cs
+++ Main.cs
@@ -5,44 +5,51 @@ using System.Collections.Generic;
 
 namespace Project2
 {
-	static class MainClass
+	static class Neo
 	{
+        // 2x4 matrix
 		static int [,]ArrayA = new int[,] {{1,1},{2,2},{3,3},{4,4}};
+        
+        // 4x2 matrix
 		static int [,]ArrayB = new int[,] {{5,4,3,2},{6,7,8,9}};
-		static int [,]ArrayC = new int[,] {{0,0},{0,0}};
 
-		public static void Main (string[] args)
-		{
+        // 4x4 end resulting matrix
+		static int [,]ArrayC = new int[,] {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
 
-			while (true)
-			{
-                var tasks = new List<Task>();
-
-                for (int i = 0; i < ArrayA.GetLength(0); i++)
-                {
-                    for (int j = 0; j < ArrayB.GetLength(1); j++)
-                    {
-                        tasks.Add(Task.Factory.StartNew(() => CalculateColumn(i, j)));
-                    }
-                }
-
-                //Block until all tasks complete.
-                Task.WaitAll(tasks.ToArray());
-
-                //see results of the task
-                for (int i = 0; i < ArrayC.GetLength(0); i++)
-                {
-                    for (int j = 0; j < ArrayC.GetLength(1); j++)
-                    {
-                        Console.WriteLine(i + " " + j + " " + ArrayC[i,j]);
-                    }
-                }
-			}
-		}
-
-        private static void CalculateColumn(int i, int j)
+        public static void Main(string[] args)
         {
-            for (int k = 0; k < ArrayB.GetLength(1); k++)
+            // creates list of pthreads to run separately
+            var pthread = new List<Task>();
+
+            for (int i = 0; i < ArrayA.GetLength(0); i++)
+            {
+                for (int j = 0; j < ArrayB.GetLength(1); j++)
+                {
+                    int x = i, y = j;
+                    pthread.Add(Task.Factory.StartNew(() => EnterTheMatrix(x, y)));
+                }
+            }
+
+            //Main thread waits until all threads complete and joins them
+            Task.WaitAll(pthread.ToArray());
+
+            //Shows results of thread multiplication
+            for (int i = 0; i < ArrayC.GetLength(0); i++)
+            {
+                for (int j = 0; j < ArrayC.GetLength(1); j++)
+                {
+                    Console.Write(ArrayC[i, j]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+        // Multiplies the RowxColumn and assigns it in ArrayC
+        private static void EnterTheMatrix(int i, int j)
+        {
+            for (int k = 0; k < ArrayB.GetLength(0); k++)
             {
                 ArrayC[i, j] += ArrayA[i, k] * ArrayB[k, j];
             }
